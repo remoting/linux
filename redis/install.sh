50ca0fba5a60e82ef058031e609a8e95d6c1ec3e
yong <remoting@qq.com>
2018-09-17 09:45:55 +0800

    consul

--- redis/install.sh
+++ redis/install.sh
@@ -3,3 +3,7 @@ mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 yum install epel-release -y
 yum install redis -y
+
+systemctl start redis.service
+systemctl enable redis.service
+
