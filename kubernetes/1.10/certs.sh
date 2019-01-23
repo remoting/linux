6ac425524dfd823b7ee28d4583c44410868372e9
yong <remoting@qq.com>
2019-01-23 19:22:06 +0800

    dashboard certs

---
+++ kubernetes/1.10/certs.sh
@@ -0,0 +1,10 @@
+cd ~ && mkdir certs && cd certs
+openssl genrsa -des3 -passout pass:x -out dashboard.pass.key 2048
+openssl rsa -passin pass:x -in dashboard.pass.key -out dashboard.key
+openssl req -new -key dashboard.key -out dashboard.csr
+openssl x509 -req -sha256 -days 365 -in dashboard.csr -signkey dashboard.key -out dashboard.crt
+kubectl delete secret kubernetes-dashboard-certs -n kube-system
+kubectl create secret generic kubernetes-dashboard-certs --from-file=$HOME/certs -n kube-system
+kubectl get pods -o wide -n kube-system
+kubectl delete pod kubernetes-dashboard-d7f7b7776-lsk68 -n kube-system
+kubectl get pods -o wide -n kube-system
