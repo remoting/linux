8442ef12f9c001d8fce91866d8861625dd28a747
yong <remoting@qq.com>
2019-04-01 19:18:39 +0800

    caddy

--- caddy/consul.sh
+++ caddy/consul.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 cd `dirname $0`
+consul::getip() {
+    host_ips=(`ip addr show|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|cut -f1 -d '/'`)
+    if [ "${host_ips[0]}" == "" ]; then
+        echo "[ERROR] get ip address error!"
+        exit 1
+    else
+        echo "${host_ips[0]}"
+    fi
+}
 consul::reg()
 {
-    local consul_addr=$1
-    local str_name=$2
-    local str_ip=$3
-    local str_port=$4
+    local consul_addr=$SPRING_CLOUD_CONSUL_HOST 
+    local consul_port=${SPRING_CLOUD_CONSUL_PORT:-"8500"}
+    local consul_token=$SPRING_CLOUD_CONSUL_TOKEN
+    local str_name=$1
+    local str_path=$2
+    local str_ip=$(consul::getip)
+    local str_port=${CTL_SERVICE_PORT:-"80"}
     local srv_config=`cat <<EOF
     { 
+        "id": "${str_name}-${str_ip//\./\-}",
         "name": "$str_name",
         "address": "$str_ip",
         "port": $str_port,
-        "meta":{
-            "sss":"1"
-        },
+        "tags": ["contextPath=$str_path"],
         "checks": [
             {
                 "DeregisterCriticalServiceAfter":"3m",
@@ -24,8 +35,11 @@ consul::reg()
         ]
     }
 EOF`
-    echo "注册服务:$srv_config" 
-    curl -H "X-Consul-Token: 60a97597-fce3-433a-932d-22c850e92df1" -s -L http://$consul_addr:8500/v1/agent/service/register -XPUT -d "${srv_config}"
+    echo "注册服务:consul $consul_addr:$consul_port,token $consul_token"
+    echo "$srv_config" 
+    curl -L --header "X-Consul-Token: $consul_token" http://$consul_addr:$consul_port/v1/agent/service/register -XPUT -d "${srv_config}"
 }
 
-consul::reg $@
+
+sleep 2
+consul::reg $1 $2
