246baf83107d2d143b809741a8121c79eb1400be
yong <remoting@qq.com>
2019-04-03 10:27:15 +0800

    demo

--- kubernetes/1.10/certs.sh
+++ kubernetes/1.10/certs.sh
@@ -8,3 +8,12 @@ kubectl create secret generic kubernetes-dashboard-certs --from-file=$HOME/certs
 kubectl get pods -o wide -n kube-system
 kubectl delete pod kubernetes-dashboard-d7f7b7776-lsk68 -n kube-system
 kubectl get pods -o wide -n kube-system
+
+
+
+kubectl create secret docker-registry regcred --docker-server=http://registry.dev.chelizitech.com --docker-username=xxx --docker-password=xxx --docker-email=r@y.cn
+
+
+
+kubectl get secrets --all-namespaces
+kubectl describe secrets kube-admin-token-fbbfv -n kube-system
